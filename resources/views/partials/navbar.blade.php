04d259ec488b624de9db1457b01b0b652f011d90
EnricFerrando <enricnavarro00@gmail.com>
2025-11-26 16:28:51 +0100

    change color navbar to green

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 {{-- filepath: c:\xampp\htdocs\DWES\Laravel\puntuacioLlibre\rankIt\resources\views\partials\navbar.blade.php --}}
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<nav class="navbar navbar-expand-lg" style="background-color: #00bcd4; margin-bottom: 50px;">
+<nav class="navbar navbar-expand-lg" style="background-color: #4ad400; margin-bottom: 50px;">
     <div class="container">
         <!-- Logo de l'app -->
         <a class="navbar-brand d-flex align-items-center" href="/">
